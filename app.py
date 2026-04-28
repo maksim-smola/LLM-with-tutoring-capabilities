0371e3be47d0d6b0e4739dac73b44564eb24a000
maksim-smola <asmolovmaksim375@gmail.com>
2026-04-28 23:53:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, jsonify
+import requests
+import os
 
 app = Flask(__name__, template_folder="templates")
 
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
 SYSTEM_PROMPT = """
 Ты образовательный ассистент.
-Отвечай понятно, структурировано и академически корректно.
+Объясняй понятным академическим языком.
+Приводи примеры.
+Отвечай структурировано.
 """
 
 @app.route("/")
@@ -13,12 +19,27 @@ def home():
 
 @app.route("/ask", methods=["POST"])
 def ask():
+
     user_question = request.json.get("question")
 
-    # Пока заглушка вместо модели
-    response = f"Учебный ответ модели на вопрос: {user_question}"
+    response = requests.post(
+        "https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": "google/gemma-4-31b-it",
+            "messages": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": user_question}
+            ]
+        }
+    )
 
-    return jsonify({"answer": response})
+    answer = response.json()["choices"][0]["message"]["content"]
+
+    return jsonify({"answer": answer})
 
 
 if __name__ == "__main__":
