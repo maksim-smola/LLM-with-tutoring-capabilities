cbbd171ecd9f8c4089128ee14d705d69e9271ae6
maksim-smola <asmolovmaksim375@gmail.com>
2026-04-28 23:32:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 SYSTEM_PROMPT = """
 Ты образовательный ассистент.
